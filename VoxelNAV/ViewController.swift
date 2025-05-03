f0ab2da7465e8d6ec3f341c0a7e6f04c614e57c9
Manish Murthy <manishmurthy080@gmail.com>
2025-05-03 15:34:07 +0100

    Initial Commit

---
+++ VoxelNAV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoxelNAV
+//
+//  Created by Manish Murthy on 03/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VoxelNAVTests/VoxelNAVTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoxelNAVTests.swift
+//  VoxelNAVTests
+//
+//  Created by Manish Murthy on 03/05/25.
+//
+
+import Testing
+@testable import VoxelNAV
+
+struct VoxelNAVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
